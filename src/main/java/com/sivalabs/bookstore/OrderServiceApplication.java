3fe30bb2963ae7423cad0b322601a4d9ff884359
Frank Heitbrock <frank.heitbrock@mailbox.org>
2026-03-31 16:24:18 +0200

    Move main class to root node

--- src/main/java/com/sivalabs/bookstore/OrderServiceApplication.java
+++ src/main/java/com/sivalabs/bookstore/OrderServiceApplication.java
@@ -1,9 +1,11 @@
-package com.sivalabs.bookstore.orderservice;
+package com.sivalabs.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
